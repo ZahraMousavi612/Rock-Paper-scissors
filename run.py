785ce3d818a117fd19e4e4a450a95530b9077e39
zahra mousavi <102756240+ZahraMousavi612@users.noreply.github.com>
2022-09-05 22:42:43 +0100

    Update run.py

--- run.py
+++ run.py
@@ -9,5 +9,15 @@ user_score = 0
 computer_score = 0
 options = ["rock", "paper", "scissors"]
 user_name = input("Welcome to Rock, Paper, Scissors Game.\nPlease Enter Your Name? ")
+
+def game(user_input):
+    """calculate the computer option"""
+    random_number = random.randint(0, 2)
+
+    # rock: 0, paper: 1, scissors: 2
+    computer_pick = options[random_number]
+    print(f'{user_name} picked {user_input} .')
+    print(f'Computer picked {computer_pick} .')
+    get_result(user_input,computer_pick)
 user_name=input("enter your name")
 Print(user_name)
