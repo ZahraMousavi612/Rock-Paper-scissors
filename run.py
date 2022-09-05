b3d02cfcf4c7d057cd2d8ba906d25869f9812725
zahra mousavi <102756240+ZahraMousavi612@users.noreply.github.com>
2022-09-05 22:42:24 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import random
+
+# modify score varible
+user_score = 0 
+computer_score = 0
+options = ["rock", "paper", "scissors"]
+user_name = input("Welcome to Rock, Paper, Scissors Game.\nPlease Enter Your Name? ")
 user_name=input("enter your name")
 Print(user_name)
